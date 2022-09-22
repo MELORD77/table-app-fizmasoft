e8e063f40632fb59bd14572f3840bb4e8a6b9a3e
Qahramonjon <qahramonmansurov98@mail.ru>
2022-09-22 12:35:07 +0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // useEffect(async () => {
+  //   const { data } = await axios.get(`http://localhost:37777/`);
+  //   console.log(data);
+  //   setData(data);
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>hello</p>
+    </>
   );
 }
 
