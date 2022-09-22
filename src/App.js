2e0c990d2aa5a2b0647cafff133ba12ab4cbab80
Qahramonjon <qahramonmansurov98@mail.ru>
2022-09-22 13:01:25 +0500

    table

--- src/App.js
+++ src/App.js
@@ -4,15 +4,59 @@ import React, { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState([]);
 
-  // useEffect(async () => {
-  //   const { data } = await axios.get(`http://localhost:37777/`);
-  //   console.log(data);
-  //   setData(data);
-  // }, []);
+  useEffect(async () => {
+    const { data } = await axios.get(
+      `https://api.lochinkoz.uz/dashboard/stat/stat_user_by_rules_with_count`
+    );
+    if (data) {
+      console.log(data.data);
+      setData(data.data);
+    }
+  }, []);
 
   return (
     <>
-      <p>hello</p>
+      <div className="container-lg search">
+        <h1>Vaqt: {data.generated_date}</h1>
+
+        <table className="table">
+          <thead>
+            <tr>
+              {data.data?.map((d, i) => (
+                <th key={`${d.id}/${i}`} scope="col">
+                  {d.title} - {d.count}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+            <tr>
+              <th scope="row">4</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
