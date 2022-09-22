a4ac4f00cb8fc346192da282c3955b97def0678a
Qahramonjon <qahramonmansurov98@mail.ru>
2022-09-22 19:11:47 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -25,29 +25,19 @@ function App() {
         <h1 className="text-center">
           Vaqt: {moment(data.generated_date).format("DD.MM.YYYY HH:mm:ss")}
         </h1>
-        {/* <div style={{ display: "flex" }}>
-          {data.data?.map((d) => (
-            <div>
-              <h6 style={{ border: "1px solid white", margin: 0 }}>
-                {d.title} - <h6 style={{color:"red"}}>{d.count}</h6>
-              </h6>
-              {d.users &&
-                d.users.map((s) => (
-                  <p style={{ border: "1px solid white", margin: '2px'  }}>{s}</p>
-                ))}
-            </div>
-          ))}
-        </div> */}
+
         <table className="table">
           <thead>
             <tr>
               {data.data?.map((d, i) => (
-                <th key={`${d.id}/${i}`} scope="col">
-                  <Tooltip title={d.title} position="top">
-                    {d.title.length > 10
-                      ? `${d.title.slice(0, 10)}...`
-                      : d.title}
-                  </Tooltip>{" "}
+                <th style={user} key={`${d.id}/${i}`} scope="col">
+                  {d.title.length > 15 ? (
+                    <Tooltip title={d.title} position="top">
+                      {d.title}
+                    </Tooltip>
+                  ) : (
+                    d.title
+                  )}{" "}
                   -{" "}
                   <span style={{ color: "red" }}>
                     {d.count ? (
@@ -66,26 +56,47 @@ function App() {
                 <th key={i}>
                   {d.users &&
                     d.users.map((s) => (
-                      <Tooltip key={s} title={s} position="top">
-                        <p
-                          style={{
-                            border: "1px solid white",
-                            margin: "5px",
-                            padding: "5px",
-                          }}
-                        >
-                          {s.length > 12 ? `${s.slice(0, 12)}...` : s}
-                        </p>
-                      </Tooltip>
+                      <p key={s} style={user}>
+                        {s.length > 18 ? (
+                          <Tooltip title={s} position="top">
+                            {s}
+                          </Tooltip>
+                        ) : (
+                          s
+                        )}
+                      </p>
                     ))}
                 </th>
               ))}
             </tr>
+            {/* <tr>
+              <th>
+                <Tooltip title="helo" position="top">
+                  <p style={{ width: "100px", overflow: "hidden" }}></p>
+                </Tooltip>
+              </th>
+            </tr> */}
           </tbody>
         </table>
       </div>
     </>
   );
 }
+const user = {
+  width: "180px",
+  color: "white",
+  border: "1px  solid white",
+  marginTop: "5px",
+  padding: "5px",
+  textOverflow: "ellipsis",
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+};
 
 export default App;
+
+{
+  /* {d.title.length > 10
+                      ? `${d.title.slice(0, 10)}...`
+                      : d.title} */
+}
