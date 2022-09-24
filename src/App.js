06ef471eea0b2f2d0f24094189272921e0862dd9
Qahramonjon <qahramonmansurov98@mail.ru>
2022-09-24 08:06:21 +0500

    new style

--- src/App.js
+++ src/App.js
@@ -7,31 +7,34 @@ import "react-tippy/dist/tippy.css";
 
 function App() {
   const [data, setData] = useState([]);
+  // const [length, setLength] = useState(null);
+  const cars = ["BMW", "Volvo", "Mini"];
 
+  // console.log(length);
   useEffect(async () => {
     const { data } = await axios.get(
       `https://api.lochinkoz.uz/dashboard/stat/stat_user_by_rules_with_count`
     );
     if (data) {
-      console.log(data.data);
-
       setData(data.data);
     }
   }, []);
 
   return (
     <>
-      <div className="container-fluid ">
-        <h1 className="text-center">
-          Vaqt: {moment(data.generated_date).format("DD.MM.YYYY HH:mm:ss")}
-        </h1>
-
-        <table className="table">
+      <div className="container-fluid m-0 p-0">
+        <div className="container-fluid bg-dark text-info p-0 m-0 w-100 h-100 ">
+          {" "}
+          <h3 className="text-center p-0 m-0">
+            Date : {moment(data.generated_date).format("DD.MM.YYYY HH:mm:ss")}
+          </h3>
+        </div>
+        {/* <table className="table">
           <thead>
             <tr>
               {data.data?.map((d, i) => (
-                <th style={user} key={`${d.id}/${i}`} scope="col">
-                  {d.title.length > 15 ? (
+                <th key={`${d.id}/${i}`} scope="col">
+                  {d.title.length > 17 ? (
                     <Tooltip title={d.title} position="top">
                       {d.title}
                     </Tooltip>
@@ -57,7 +60,7 @@ function App() {
                   {d.users &&
                     d.users.map((s) => (
                       <p key={s} style={user}>
-                        {s.length > 18 ? (
+                        {s.length > 21 ? (
                           <Tooltip title={s} position="top">
                             {s}
                           </Tooltip>
@@ -69,25 +72,74 @@ function App() {
                 </th>
               ))}
             </tr>
-            {/* <tr>
-              <th>
-                <Tooltip title="helo" position="top">
-                  <p style={{ width: "100px", overflow: "hidden" }}></p>
-                </Tooltip>
-              </th>
-            </tr> */}
           </tbody>
-        </table>
+        </table> */}
+
+        <div
+          className=" container-fluid d-inline-flex p-0 m-auto border "
+          style={{ height: "900px" }}
+        >
+          {data.data?.map((d, i) => (
+            <div className="container-fluid m-0 p-1 bg-dark">
+              <ul className="list-group" key={i}>
+                <p className="list-group-item m-0 p-auto border  bg-secondary border-info d-flex">
+                  <p style={user}>
+                    {" "}
+                    {d.title} {""}{" "}
+                  </p>
+                  {d.count ? (
+                    <span className=" m-auto  count badge bg-danger ">
+                      {d.count}
+                    </span>
+                  ) : (
+                    <span className="count text-dark badge m-auto  bg-warning">
+                      0
+                    </span>
+                  )}
+                </p>
+
+                {/* <p>{setLength(d.users)}</p> */}
+                <div>
+                  {" "}
+                  {d.users &&
+                    d.users.map((s) => (
+                      <span
+                        className="list-group-item bg-dark border border-info text-white"
+                        key={s}
+                      >
+                        {s.length > 18 ? (
+                          <Tooltip title={s} position="top">
+                            <p
+                              className="text-nowrap bd-highlight overflow-hidden text-truncate"
+                              style={{ width: "150px" }}
+                            >
+                              {" "}
+                              {s}
+                            </p>
+                          </Tooltip>
+                        ) : (
+                          s
+                        )}
+                      </span>
+                    ))}
+                </div>
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
 }
 const user = {
-  width: "180px",
+  display: "inlineBlock",
+  width: "100px",
   color: "white",
-  border: "1px  solid white",
-  marginTop: "5px",
-  padding: "5px",
+
+  margin: "0px",
+  padding: "0px",
+  // marginTop: "5px",
+  // padding: "auto",
   textOverflow: "ellipsis",
   overflow: "hidden",
   whiteSpace: "nowrap",
